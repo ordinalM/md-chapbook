cef3ce6e47f4e757d92999f42bd03dda6710697a
OrdinalM <ordinal@ordinalmalaprop.com>
2021-11-13 21:44:49 +0000

    Use system TMPDIR, output to STDOUT

--- bin/make-pocketfold.sh
+++ bin/make-pocketfold.sh
@@ -10,27 +10,22 @@ pandoc -v >/dev/null || die_with "Pandoc not installed"
 
 [[ -f "$1" ]] || die_with "First argument must be a markdown file"
 
-TMPDIR=".tmp"
-[[ -d "$TMPDIR" ]] || mkdir "$TMPDIR" || die_with "Could not create $TMPDIR"
+TMPDIR_PAGES="$TMPDIR/md-booklet"
+[[ -d "$TMPDIR_PAGES" ]] || mkdir "$TMPDIR_PAGES" || die_with "Could not create $TMPDIR_PAGES"
 HR_REGEX="/^---$/"
 
-csplit -s -f "$TMPDIR/page" "$1" "$HR_REGEX" "{6}" || die_with "Splitting files failed - are there enough pages?"
+csplit -s -f "$TMPDIR_PAGES/page" "$1" "$HR_REGEX" "{6}" || die_with "Splitting files failed - are there enough pages?"
 
-OUTFILE="output/$(basename "$1" | sed -E "s/\.(txt|md|markdown)$//").html"
-cp "html/header.html" "$OUTFILE"
+cat "html/header.html"
 
 I=1
-find "$TMPDIR" -type f | sort | while read -r PAGEFILE; do
-  {
+find "$TMPDIR_PAGES" -type f | sort | while read -r PAGEFILE; do
     echo "<div class=\"page p$I\">"
     I=$((I+1))
     sed "s$HR_REGEX/" "$PAGEFILE" | pandoc -f markdown -t html5
     echo '</div>'
-  } >>"$OUTFILE"
 done
 
-cat "html/footer.html" >> "$OUTFILE"
+cat "html/footer.html"
 
-rm -fr "$TMPDIR"
-
-echo "Created $OUTFILE"
+rm -fr "$TMPDIR_PAGES"
